545d2c0533b5aab256a12fc6bb08584b8160a0f5
NadiiaMa <n.maklyak@yahoo.com>
2024-06-07 13:14:37 +0200

    142

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,6 +1,26 @@
-def test_cgeck_math():
-    assert 7 * 7 == 49
+import pytest
 
 
-def test_cgeck_78():
-    assert 7 * 8 == 56
+class User:
+
+    def __init__(self) -> None:
+        self.name = "Sergii"
+        self.second_name = "Butenko"
+
+
+@pytest.fixture
+def user():
+    yield User()
+
+
+def test_remove_name(user):
+    user.name = ''
+    assert user.name == ''
+
+
+def test_name(user):
+    assert user.name == "Sergii"
+
+
+def test_second_name(user):
+    assert user.second_name == "Butenko"
